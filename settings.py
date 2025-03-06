5c425ef97237cf8379b499b2602bbd4047094683
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-03-06 15:53:23 +0100

    settings: added radlex graph dir and path variables

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+RADLEX_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ontology', 'data')
+RADLEX_DATA = os.path.join(RADLEX_DATA_DIR, 'RadLex.owl')
 
-RADLEX_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ontology', 'data')
-RADLEX_DATA = os.path.join(RADLEX_DATA_PATH, 'RadLex.owl')
+RADLEX_GRAPH_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ontology')
+RADLEX_GRAPH = os.path.join(RADLEX_GRAPH_DIR, 'radlex_graph.json')
