7eb0237846b91e6d3f51780faf0cc2854ee5ce50
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-03-06 22:45:03 +0100

    settings: added filter_radlex_json path variable

--- settings.py
+++ settings.py
@@ -4,3 +4,5 @@ RADLEX_DATA = os.path.join(RADLEX_DATA_DIR, 'RadLex.owl')
 
 RADLEX_GRAPH_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ontology')
 RADLEX_GRAPH = os.path.join(RADLEX_GRAPH_DIR, 'radlex_graph.json')
+
+FILTER_RADLEX_JSON = os.path.join(RADLEX_DATA_DIR, 'filter.json')
