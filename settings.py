40a23037a4f43a0b528a7cc90ce6adce0146dc87
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-03-08 00:21:00 +0100

    settings.py: added variables for model, xai and preprocess settings

--- settings.py
+++ settings.py
@@ -9,3 +9,26 @@ FILTER_RADLEX_JSON = os.path.join(RADLEX_DATA_DIR, 'filter.json')
 
 # Report data
 MIMIC_REPORT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mimic')
+
+# Dataset path
+DATASET_PATH = "/path/to/mimic_cxr_subset/"
+
+# ====== PARAMETERS FOR FEATURE EXTRACTION IN NLP (Graph) ======
+
+NUM_WORKERS = 8 # Number of parallel workers for processing (Threads)
+
+# ===== Hyperparameters for Training =====
+
+NUM_EPOCHS = 10
+LEARNING_RATE_TRANSFORMER = 1e-5 # For Transformer Blocks
+LEARNING_RATE_CLASSIFIER = 1e-4 # For Classifier Head
+BATCH_SIZE = 16
+
+UNBLOCKED_LEVELS = 3 # Number of unblocked levels in the Swin Transformer (from end to start)
+
+SWIN_MODEL_SAVE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model', 'swin_model.pth')
+SWIN_STATS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model', 'swin_stats.json')
+
+# ====== PARAMETERS FOR FEATURE EXTRACTION IN XAI (Attention Map) ======
+
+ATTENTION_MAP_THRESHOLD = 0.5 # Threshold for attention map analysis
