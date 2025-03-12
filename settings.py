0bafc5edf139cf2ff11390d7b240f0f2615eb7c1
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-03-12 21:37:45 +0100

    settings: added dataset path and csv info, similarity threshold for report keywords

--- settings.py
+++ settings.py
@@ -11,13 +11,16 @@ FILTER_RADLEX_JSON = os.path.join(RADLEX_DATA_DIR, 'filter.json')
 MIMIC_REPORT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mimic')
 
 # Dataset path
-DATASET_PATH = "/path/to/mimic_cxr_subset/"
+DATASET_PATH = os.environ.get('MIMIC_DATASET_PATH')
+
+# Directory where csv files are stored, containing the dataset information
+DATASET_INFO_CSV_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mimic', 'info')
 
 # ====== PARAMETERS FOR FEATURE EXTRACTION IN NLP (Graph) ======
 
 NUM_WORKERS = 8 # Number of parallel workers for processing (Threads)
 
-# ===== Hyperparameters for Training =====
+# ======================== Hyperparameters for TRAINING =================================
 
 NUM_EPOCHS = 10
 LEARNING_RATE_TRANSFORMER = 1e-5 # For Transformer Blocks
@@ -32,3 +35,9 @@ SWIN_STATS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mode
 # ====== PARAMETERS FOR FEATURE EXTRACTION IN XAI (Attention Map) ======
 
 ATTENTION_MAP_THRESHOLD = 0.5 # Threshold for attention map analysis
+
+
+# ===== PARAMETERS FOR SIMILARITY CALCULATION ======
+
+# Threshold for similarity calculation between graph nodes and medical reports
+SIMILARITY_THRESHOLD = 0.7
