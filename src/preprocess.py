0053c3c74410dd3373a7e90d435ac71b3fc6c467
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-03-14 18:48:45 +0100

    preprocess: added ImagePreprocessor class to be used as Dataset class in DataLoader

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,6 @@
-import torch
 import torchvision.transforms as transforms
+from torch.utils.data import Dataset
 from PIL import Image
-import numpy as np
 
 
 # Function to resize while maintaining aspect ratio and add padding
@@ -19,17 +18,50 @@ def preprocess_image(image_path):
 
     img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
 
-    # Creation of a white image (padding)
+    # Creation of a white image (padding) (black in this case)
     padded_img = Image.new("RGB", (256, 256), (0, 0, 0))
     padded_img.paste(img, ((256 - new_width) // 2, (256 - new_height) // 2))
 
     # Convert to tensor and normalize for PyTorch
     transform = transforms.Compose([
+        #transforms.Resize(image_size),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Standard Normalization
     ])
 
     return transform(padded_img).unsqueeze(0)  # Adds batch dimension
+
+
+class ImagePreprocessor(Dataset):
+    """
+        Custom dataset class for loading and preprocessing images.
+        Args:
+            image_paths (list): List of paths to the images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+            image_size (tuple): Desired output size of the image.
+    """
+    def __init__(self, image_paths, transform = None, image_size=(256, 256)):
+        self.image_size = image_size
+        self.image_paths = image_paths
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx (int): Index of the image to be fetched.
+        Returns:
+            Tensor: Preprocessed image tensor.
+        """
+        img_pth = self.image_paths[idx]
+        if self.transform:
+            # use the transform defined in the constructor
+            return self.transform(img_pth)
+        else:
+            return preprocess_image(img_pth)
 
 # =============================================
 
